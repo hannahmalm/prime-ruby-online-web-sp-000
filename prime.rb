5712c8cbba0fa40236f2fa8b04e2c295681947df
Hannah Malm <hannahemalm616@gmail.com>
2021-01-27 02:46:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -12,6 +12,6 @@
 # a prime number cannot be negative 
 
 def prime?(number)
-  #if number is >= 2 AND 2 can be divided by 2 without a remainder, then iterate over the numbers and ensure that the number you are feeding the code is divisible by the 
+  #if number is >= 2 AND 2 can be divided by 2 without a remainder, then iterate over the numbers and ensure that the number you are feeding the code is divisible by the iteratenumber and that when you divide it, it returns 0 
   number >= 2 && (2..number/2).none? { |iteratenumber| number % iteratenumber == 0}
 end   
