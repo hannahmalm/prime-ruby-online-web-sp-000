fa0a79dc6fa5afdcc22d587cbec1305cead8394f
Hannah Malm <hannahemalm616@gmail.com>
2021-01-27 02:40:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+  # create a range starting with 2 and subtract one from what ever the number is in the range
+  # do |n| iterates through the number range
+ (2..(number - 1)).each do |n|
+   # if the given number is entirely divisible (there will be no remainder)
+   return false if number % n == 0 
+  end 
+  true
+end 
+#a prime number is a number that can only be divided by 1 and by itself
+# a prime number cannot be negative 
